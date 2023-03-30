25ca437519958dfd904d1a0096b1062ccf6d6e3a
TotsiNor <sondretotsdal@gmail.com>
2023-03-30 14:19:55 +0200

    updates

--- src/motion_pkg/scripts/OAK_pro_camera_test.py
+++ src/motion_pkg/scripts/OAK_pro_camera_test.py
@@ -1,10 +1,151 @@
+#!/usr/bin/env python3  
 # Script for testing the OAK D poe pro camera
 
 # importing modules
 import cv2
+import sys
 import depthai as dai
 import os
 import numpy as np
+import cv2.aruco as aruco
+import math as m
+from tf.transformations import quaternion_matrix
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import tf2_ros
+ 
+
+
+#Tf node
+rospy.init_node('tf_listener', anonymous=True)
+tfBuffer = tf2_ros.Buffer()
+listener = tf2_ros.TransformListener(tfBuffer)
+rate = rospy.Rate(10.0)
+'''
+
+while not rospy.is_shutdown():
+    try:
+        robTransform = listener.lookupTransform('/base','/tool0',rospy.Time(0))
+    except:
+        continue
+    '''
+allCharucoCorners = []
+allCharucoIds = []
+allRotationVectors = []
+allTranlationVectors = []
+
+
+def quat2ang(quat):
+    #normalise quaternions
+    x = quat.x
+    y = quat.y
+    z = quat.z
+    w = quat.w
+    
+    norm = m.sqrt(x*x + y*y + z*z + w*w)
+    x = quat.x/norm
+    y = quat.y/norm
+    z = quat.z/norm
+    w = quat.w/norm
+    #convert to angle
+    angle = 2*m.acos(w)
+    s = m.sqrt(1-w*w)
+    if(s<0.00001):
+        #rotational direction can be ignored because of low angle
+        rx = 1  
+        ry = 0
+        rz = 0
+    else:
+        rx = x / s
+        ry = y / s
+        rz = z / s
+    #normalize rotation
+    norm = m.sqrt(rx*rx+ry*ry+rz*rz)
+    rx = rx /norm*angle
+    ry = ry/norm*angle
+    rz = rz/norm*angle
+    ang = [[rx,0,0],[0,ry,0],[0,0,rz]]
+    return ang
+
+def ang2quat(ang):
+    rx = ang[0][0]
+    ry = ang[1][0]
+    rz = ang[2][0]
+    
+    angle = m.sqrt(rx*rx+ry*ry+rz*rz)
+    quat = [0.0, 0.0, 0.0 ,0.0]
+    quat[0] = rx/angle*m.sin(angle/2)
+    quat[1] = ry/angle*m.sin(angle/2)
+    quat[2] = rz/angle*m.sin(angle/2)
+    quat[3] = m.cos(angle/2)
+    
+    return quat
+
+def transformToRotationVector(transform):
+    global allRotationVectors
+    global allTranlationVectors
+    
+    
+    rotation = transform.transform.rotation
+    translation = transform.transform.translation
+    
+    rvec = quat2ang(rotation)
+    allRotationVectors.append(rvec)
+    tvec = [[translation.x], [translation.y], [translation.z]]
+    allTranlationVectors.append(tvec)
+    return
+
+
+def addSample():
+    
+    if charucoIds is not None:
+        global allCharucoCorners
+        global allCharucoIds
+        allCharucoCorners.append(charucoCorners)
+        allCharucoIds.append(charucoIds)
+        robTransform = tfBuffer.lookup_transform('tool0', 'base_link', rospy.Time())
+        transformToRotationVector(robTransform)
+        print("Sample saved.")
+    else:
+        print("All CharucoCorners not detected. ")
+        
+def calibrate():
+    err, cameraMatrix, distCoeffs, r_target2cam, t_target2cam= aruco.calibrateCameraCharuco(allCharucoCorners, allCharucoIds, board, imageSize, None, None)
+    new_r_target2cam = []
+    
+    
+    for row in r_target2cam:
+        rotmat, jacob = cv2.Rodrigues(row)
+        new_r_target2cam.append(rotmat)
+    r_target2cam = new_r_target2cam
+    
+    R_gripper2base = np.array(allRotationVectors)
+    
+    T_gripper2base = np.array(allTranlationVectors)
+    
+    hand_to_eye = True
+    if hand_to_eye:
+        gripper2baseR = R_gripper2base
+        gripper2baseT = T_gripper2base
+        
+        
+        
+        R_gripper2base = r_target2cam
+        T_gripper2base = t_target2cam
+        
+        r_target2cam = gripper2baseR
+        t_target2cam = gripper2baseT
+    
+        
+        
+        
+    
+    
+    R_camera2gripper, T_camera2gripper = cv2.calibrateHandEye(R_gripper2base, T_gripper2base, r_target2cam, t_target2cam, method=cv2.CALIB_HAND_EYE_TSAI)
+    print(R_camera2gripper, T_camera2gripper)
+    return   
 
 # create pipeline
 pipeline = dai.Pipeline()
@@ -26,7 +167,7 @@ xout.setStreamName("mjpeg")
 videoEnc.bitstream.link(xout.input)
 
 # TODO: if it is needed to set a manual focus
-# camRgb.initialControl.setManualFocus(180)
+#camRgb.initialControl.setManualFocus(180)
 
 #* depth map
 
@@ -35,7 +176,7 @@ monoLeft = pipeline.create(dai.node.MonoCamera)
 monoRight = pipeline.create(dai.node.MonoCamera)
 stereo = pipeline.create(dai.node.StereoDepth)
 
-camRgb.initialControl.setManualFocus(120)
+camRgb.initialControl.setManualFocus(200)
 
 # properties (resolution can be changed)
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
@@ -61,7 +202,7 @@ stereo.depth.link(xoutDepth.input)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutDepth.setStreamName("disp")
 stereo.disparity.link(xoutDepth.input)
-deviceInfo = dai.DeviceInfo('169.254.1.222') #Can change ip to xx.xxx.xxx.200
+deviceInfo = dai.DeviceInfo('169.254.1.222') #Can change ip to xx.xxx.xxx.200 169.254.1.200
 
 # connect to device and start pipeline
 with dai.Device(pipeline, deviceInfo) as device:
@@ -93,20 +234,51 @@ with dai.Device(pipeline, deviceInfo) as device:
         disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
 
         # show rgb image stream
-        print("[INFO] Loading image stream...")
-        cv2.imshow("MJPEG", frame)
+        #print("[INFO] Loading image stream...")
+        #cv2.imshow("MJPEG", frame)
         
         # show depth frame
         #cv2.imshow("depth", disp)
         
         #cv2.imshow("depth_frame", depthFrame)
         
-        print(frame.shape)
-        print(depthFrame.shape)
+        #print(frame.shape)
+        #print(depthFrame.shape)
+        
         
 
-        if cv2.waitKey(1) == ord('q'):
-            break
-             
         
+        frameCopy = frame.copy()
+        
+        dictionary = aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
+        board = aruco.CharucoBoard_create(7, 5, 0.04, 0.02, dictionary)
+        params = aruco.DetectorParameters_create()
+        params.cornerRefinementMethod = aruco.CORNER_REFINE_NONE
+        height, width = frameCopy.shape[:2]
+        imageSize = (width, height)
+
+        
+        markerCorners, markerIds, _ = aruco.detectMarkers(frameCopy, dictionary, parameters=params)
+        if markerIds is not None and len(markerIds) > 0:
+            aruco.drawDetectedMarkers(frameCopy, markerCorners, markerIds)
+            retval, charucoCorners, charucoIds  = aruco.interpolateCornersCharuco(markerCorners, markerIds, frameCopy, board)  
+            
+            if isinstance(charucoCorners, np.ndarray):
+                if charucoIds is not None and charucoCorners is not None and charucoCorners.shape[0] != 0:
+                    aruco.drawDetectedCornersCharuco(frameCopy, charucoCorners, charucoIds, (0, 255, 0))
+        cv2.imshow('out', frameCopy)        
+        
+        
+            
+        
+  
+            
+        
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('s'): # Add sample 
+            addSample()
+        elif key == ord('c'): #Calibrate
+            calibrate()
         
--- src/motion_pkg/scripts/OAK_pro_edit.py
+++ src/motion_pkg/scripts/OAK_pro_edit.py
@@ -3,6 +3,7 @@
 
 # importing modules
 import cv2
+import sys
 import depthai as dai
 import os
 import numpy as np
@@ -13,50 +14,41 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-import tf
+import tf2_ros
+ 
 
-#Moveit node
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node("moveRobot", anonymous=True)
-
-
-robot = moveit_commander.RobotCommander()
-scene = moveit_commander.PlanningSceneInterface()
-group_name = "manipulator"
-move_group = moveit_commander.MoveGroupCommander(group_name)
-
-
-# Create a DisplayTrajectory ROS publisher which is used to display trajectories in Rviz
-display_trajectory_publisher = rospy.Publisher(
-    "/move_group/display_planned_path",
-    moveit_msgs.msg.DisplayTrajectory,
-    queue_size=20,
-)
 
 #Tf node
-rospy.init_node('tf_listener')
-listener = tf.TransformListener
-
+rospy.init_node('tf_listener', anonymous=True)
+tfBuffer = tf2_ros.Buffer()
+listener = tf2_ros.TransformListener(tfBuffer)
 rate = rospy.Rate(10.0)
+'''
+
 while not rospy.is_shutdown():
     try:
-        robTransform = listener.lookupTransform('/base_lin','/end_effector',rospy.Time(0))
+        robTransform = listener.lookupTransform('/base','/tool0',rospy.Time(0))
     except:
         continue
-    
+    '''
 allCharucoCorners = np.array([])
 allCharucoIds = np.array([])
 allRotationVectors = np.array([])
-allTranlationVectors = np.array([]
-                                
-                                )
+allTranlationVectors = np.array([])
+
+
 def quat2ang(quat):
     #normalise quaternions
+    x = quat.x
+    y = quat.y
+    z = quat.z
+    w = quat.w
+    
     norm = m.sqrt(x*x + y*y + z*z + w*w)
-    x = quat[0]/norm
-    y = quat[1]/norm
-    z = quat[2]/norm
-    w = quat[3]/norm
+    x = quat.x/norm
+    y = quat.y/norm
+    z = quat.z/norm
+    w = quat.w/norm
     #convert to angle
     angle = 2*m.acos(w)
     s = m.sqrt(1-w*w)
@@ -83,7 +75,7 @@ def ang2quat(ang):
     rz = ang[2][0]
     
     angle = m.sqrt(rx*rx+ry*ry+rz*rz)
-    quat = []
+    quat = [0.0, 0.0, 0.0 ,0.0]
     quat[0] = rx/angle*m.sin(angle/2)
     quat[1] = ry/angle*m.sin(angle/2)
     quat[2] = rz/angle*m.sin(angle/2)
@@ -100,10 +92,10 @@ def transformToRotationVector(transform):
     translation = transform.transform.translation
     
     rvec = quat2ang(rotation)
-    allRotationVectors.append(rvec)
+    allRotationVectors = np.append(allRotationVectors,rvec)
     
     tvec = np.array([[translation.x], [translation.y], [translation.z]])
-    allTranlationVectors.append(tvec)
+    allTranlationVectors = np.append(allTranlationVectors, tvec)
     return
 
 
@@ -112,18 +104,21 @@ def addSample():
     if charucoIds is not None:
         global allCharucoCorners
         global allCharucoIds
-        
+
         allCharucoCorners = np.append(allCharucoCorners, charucoCorners)
         allCharucoIds = np.append(allCharucoIds,charucoIds)
+        robTransform = tfBuffer.lookup_transform('base', 'tool0', rospy.Time())
+        objPoints, imgPoints = board.matchImagePoints(markerCorners,markerIds)
+        
         transformToRotationVector(robTransform)
         print("Sample saved.")
     else:
         print("All CharucoCorners not detected. ")
         
 def calibrate():
-    
-    cameraMatrix, distCoeffs, reprojError, r_camera2end, t_camera2end = cv2.calibrateCameraCharuco(allCharucoCorners, allCharucoIds, board, imageSize)
-    R_camera2gripper, T_camera2gripper = cv2.calibrateHandEye(allRotationVectors, allTranlationVectors, r_camera2end, t_camera2end)
+   
+    retval, cameraMatrix, distCoeffs, r_camera2marker, t_camera2marker = board.calibrateCameraCharuco(allCharucoCorners, allCharucoIds, board, imageSize, None, None)
+    R_camera2gripper, T_camera2gripper = aruco.calibrateHandEye(allRotationVectors, allTranlationVectors, r_camera2marker, t_camera2marker, method='CALIB_HAND_EYE_TSAI')
     
     
     return   
@@ -148,7 +143,7 @@ xout.setStreamName("mjpeg")
 videoEnc.bitstream.link(xout.input)
 
 # TODO: if it is needed to set a manual focus
-# camRgb.initialControl.setManualFocus(180)
+#camRgb.initialControl.setManualFocus(180)
 
 #* depth map
 
@@ -157,7 +152,7 @@ monoLeft = pipeline.create(dai.node.MonoCamera)
 monoRight = pipeline.create(dai.node.MonoCamera)
 stereo = pipeline.create(dai.node.StereoDepth)
 
-camRgb.initialControl.setManualFocus(120)
+camRgb.initialControl.setManualFocus(200)
 
 # properties (resolution can be changed)
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
@@ -183,7 +178,7 @@ stereo.depth.link(xoutDepth.input)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
 xoutDepth.setStreamName("disp")
 stereo.disparity.link(xoutDepth.input)
-deviceInfo = dai.DeviceInfo('169.254.1.222') #Can change ip to xx.xxx.xxx.200
+deviceInfo = dai.DeviceInfo('169.254.1.222') #Can change ip to xx.xxx.xxx.200 169.254.1.200
 
 # connect to device and start pipeline
 with dai.Device(pipeline, deviceInfo) as device:
@@ -204,7 +199,6 @@ with dai.Device(pipeline, deviceInfo) as device:
         # RGB image data
         data = q.get().getData()  # blocking call, will wait until new data has arrived
         frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
-        
         # depth image data
         depthFrame = depthQueue.get().getFrame()
         # depth_frame = cv2.imdecode(depthFrame, cv2.IMREAD_COLOR)
@@ -230,23 +224,19 @@ with dai.Device(pipeline, deviceInfo) as device:
 
         
         frameCopy = frame.copy()
-        
         dictionary = aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
-        board = aruco.CharucoBoard_create(7, 5, 0.04, 0.02, dictionary)
-        params = aruco.DetectorParameters_create()
-        params.cornerRefinementMethod = aruco.CORNER_REFINE_NONE
+        board = aruco.CharucoBoard((7, 5), 0.04, 0.02, dictionary)
+        params = aruco.DetectorParameters()
         height, width = frameCopy.shape[:2]
         imageSize = [width, height]
 
         
-        markerCorners, markerIds, _ = aruco.detectMarkers(frameCopy, dictionary, parameters=params)
-        if markerIds is not None and len(markerIds) > 0:
+        detector = aruco.CharucoDetector(board)
+        charucoCorners, charucoIds, markerCorners, markerIds = detector.detectBoard(frameCopy)
+        if markerIds is not None and len(markerIds) > 0 and charucoIds is not None and charucoCorners is not None:
             aruco.drawDetectedMarkers(frameCopy, markerCorners, markerIds)
-            retval, charucoCorners, charucoIds  = aruco.interpolateCornersCharuco(markerCorners, markerIds, frameCopy, board)  
+            aruco.drawDetectedCornersCharuco(frameCopy, charucoCorners, charucoIds, (0, 255, 0))
             
-            if isinstance(charucoCorners, np.ndarray):
-                if charucoIds is not None and charucoCorners is not None and charucoCorners.shape[0] != 0:
-                    aruco.drawDetectedCornersCharuco(frameCopy, charucoCorners, charucoIds, (0, 255, 0))
         cv2.imshow('out', frameCopy)        
         
         
@@ -260,8 +250,6 @@ with dai.Device(pipeline, deviceInfo) as device:
             break
         elif key == ord('s'): # Add sample 
             addSample()
-            print(allCharucoCorners)
-            print(allCharucoIds)
         elif key == ord('c'): #Calibrate
             calibrate()
         
